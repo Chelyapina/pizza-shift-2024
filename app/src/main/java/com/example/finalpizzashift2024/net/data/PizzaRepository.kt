629f35efd633a4f1a8df01549522b51d8b6c2ca6
Chelyapina <katechelya@gmail.com>
2024-07-07 22:00:01 +0700

    alert for one pizza

--- app/src/main/java/com/example/finalpizzashift2024/net/data/PizzaRepository.kt
+++ app/src/main/java/com/example/finalpizzashift2024/net/data/PizzaRepository.kt
@@ -15,7 +15,16 @@ class NetPizzaRepository(
             name = catalog.name,
             description = catalog.description,
             img = "https://shift-backend.onrender.com" + catalog.img,
-            minCost = catalog.sizes.find { it.name == NameSize.SMALL }?.price
+            minCost = catalog.sizes.find { it.name == NameSize.SMALL }?.price,
+            doughs = catalog.doughs,
+            sizes = catalog.sizes,
+            dataToppings = catalog.toppings.map { ingredients ->
+                DataToppings(
+                    name = ingredients.name,
+                    cost = ingredients.cost,
+                    img = "https://shift-backend.onrender.com" + ingredients.img
+                )
+            }
         )
 
     }
--- app/src/main/java/com/example/finalpizzashift2024/ui/theme/screens/PizzaScreen.kt
+++ app/src/main/java/com/example/finalpizzashift2024/ui/theme/screens/PizzaScreen.kt
@@ -1,20 +1,31 @@
 package com.example.finalpizzashift2024.ui.theme.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -25,8 +36,10 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.finalpizzashift2024.R
+import com.example.finalpizzashift2024.net.data.DataToppings
 import com.example.finalpizzashift2024.ui.theme.Typography
 import com.example.finalpizzashift2024.net.data.OnePizzaInfo
+
 
 @Composable
 fun PizzaScreen(pizza: List<OnePizzaInfo>, modifier: Modifier) {
@@ -51,14 +64,21 @@ fun PizzaScreen(pizza: List<OnePizzaInfo>, modifier: Modifier) {
 
 @Composable
 fun PizzaCard(pizza: OnePizzaInfo, modifier: Modifier = Modifier) {
+    var showDialog by remember { mutableStateOf(false) }
+
     Card(
         modifier = modifier
             .padding(15.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable { showDialog = true },
         colors = CardDefaults.cardColors(
             containerColor = Color.Transparent
         )
     ) {
+        if (showDialog) {
+            PizzaAlert(pizza, onDismiss = { showDialog = false })
+        }
+
         Row(horizontalArrangement = Arrangement.spacedBy(15.dp)) {
             AsyncImage(
                 modifier = modifier.size(160.dp, 160.dp),
@@ -100,6 +120,94 @@ fun PizzaCard(pizza: OnePizzaInfo, modifier: Modifier = Modifier) {
                 }
 
             }
+        }
+    }
+}
+
+@Composable
+fun PizzaAlert(pizza: OnePizzaInfo, onDismiss: () -> Unit){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(stringResource(id =R.string.main_page_title)) },
+        text = {
+            Column {
+                pizza.name?.let { Text(it, style = Typography.titleLarge) }
+
+                AsyncImage(
+                    modifier = Modifier.size(220.dp,220.dp),
+                    model = ImageRequest.Builder(context = LocalContext.current)
+                        .data(pizza.img)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = stringResource(id = R.string.content_description),
+                    contentScale = ContentScale.Crop
+                )
+
+                //Spacer(modifier = Modifier.height(16.dp))
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(1),
+                    contentPadding = PaddingValues(4.dp)
+                ) {
+                    itemsIndexed(pizza.dataToppings ?: emptyList()) { _, ingredient ->
+                        IngredientCard(ingredient = ingredient)
+                    }
+                }
+            }
+//            LazyVerticalGrid(
+//                columns = GridCells.Fixed(1),
+//                contentPadding = PaddingValues(4.dp)
+//            ) {
+//                itemsIndexed(pizza) { _, piz ->
+//                    PizzaCard(pizza = piz)
+//                }
+//            }
+        },
+
+        confirmButton = {
+            Button(onClick = onDismiss) {
+                Text("Close")
+            }
+        },
+        //style = Typography.titleLarge,
+    )
+}
+
+
+@Composable
+fun IngredientCard(
+    ingredient: DataToppings
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        //elevation = 4.dp,
+        //shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                AsyncImage(
+                    model = ingredient.img,
+                    contentDescription = ingredient.name.toString(),
+                    modifier = Modifier.size(48.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = ingredient.name.toString(),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+            Text(
+                text = ingredient.cost?.toString() ?: "N/A",
+                style = MaterialTheme.typography.labelLarge
+            )
         }
     }
 }
